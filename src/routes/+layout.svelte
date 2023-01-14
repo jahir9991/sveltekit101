a45b152701a6fd04cf9c1d791476cf33fd60dd5f
MD JAHIR ALAM RAJU <jahir9991@MDs-MacBook-Air.local>
2023-01-14 18:28:22 +0600

    gasp

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,8 @@
 
 <ul class="inline-flex">
 	<li class="p-4 " class:bg-red-900={$page.url.pathname === '/'}><a href="/">home</a></li>
+	<li class="p-4 " class:bg-red-900={$page.url.pathname === '/mouse'}><a href="/mouse">mouse</a></li>
+	<li class="p-4 " class:bg-red-900={$page.url.pathname === '/test'}><a href="/test">test</a></li>
 	<li class="p-4" class:bg-red-900={$page.url.pathname === '/products'}>
 		<a  href="/products">products</a>
 	</li>
